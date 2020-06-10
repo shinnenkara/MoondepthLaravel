c3155ef54ccbf205ab910f7ba41e57defa0590c8
Alexey Kharchenko <doom9936.1999@gmail.com>
2020-06-10 10:20:42 +0300

    Stable version 0.5

---
+++ moondepth-prod/app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use App\Board;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected static function getAllBoards() {
+
+        return Board::select('headline', 'description', 'type')->orderBy('headline', 'asc')->get();
+    }
+}
---
+++ moondepth-prod/app/Http/Controllers/MessageController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\Thread;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+
+    }
+
+    /**
+     * Get the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return array|false|string
+     */
+    public function get($board, $thread, Message $message)
+    {
+        $user = $message->user;
+        $filesPath = substr(Storage::disk('s3')->url('/'), 0, -1);
+        $files = array();
+
+        if(!$message->files->isEmpty()) {
+            $files = $message->files;
+        }
+
+        return array('message' => $message, 'user' => $user, 'filesPath' => $filesPath, 'files' => $files);
+    }
+
+    /**
+     * Get all specified resources.
+     *
+     * @param  \App\Thread  $thread
+     * @return array|false|string
+     */
+    public function all($board, Thread $thread)
+    {
+        $messages = $thread->messages;
+
+        return array('messages' => $messages);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
